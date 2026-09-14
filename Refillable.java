b2161c47a8ee8c8f136e48596b7103645344e87f
Andy <rothstein.andy@gmail.com>
2026-09-14 12:27:44 -0700

    jhvvhvj

--- Refillable.java
+++ Refillable.java
@@ -4,33 +4,50 @@ public class Refillable {
 
 	private int myAmount;
 	private int myUseUpCallCount;
-	private int myTotalUse; 
-	
+	private int myTotalUse;
+
 	// Initialize a Refillable object having initialAmount units.
-	public Refillable (int initialAmount) {
-      myAmount = initialAmount;
-      myUseUpCallCount = 0; 
-	  myTotalUse = 0; 
+	public Refillable(int initialAmount) {
+		if (initialAmount > 0)
+			myAmount = initialAmount;
+		// myUseUpCallCount = 0;
+		// myTotalUse = 0;
 	}
-	
+
+
 	// Use amount units.
-	public void useUp (int amount) {
-    	myAmount = myAmount - amount; 
-		myTotalUse = myTotalUse + amount;
-        myUseUpCallCount = myUseUpCallCount + 1; 
+	public void useUp(int amount) {
+		if(0<amount){
+			if(amount>myAmount){
+				System.out.println("we don't have that much");
+				amount = myAmount;
+			}
+			myAmount -=  amount;
+			myTotalUse +=  amount;
+			myUseUpCallCount ++;
+				
+		}
+			
+		
 	}
-	
+
 	// Add amount to the current amount.
-	public void refill (int amount) {
-  		myAmount = myAmount + amount;
-  }
-	
+	public void refill(int amount) {
+		if (0 < amount)
+			myAmount = myAmount + amount;
+	}
+
 	// Return the current amount.
-	public int currentAmount ( ) {
+	public int currentAmount() {
 		return myAmount;
 	}
 
-	public double averageUse ( ) {
-  		return myTotalUse / (myUseUpCallCount * 1.0);
-	} 
+	public double averageUse() {
+		if(myUseUpCallCount>0)
+			return myTotalUse / (myUseUpCallCount * 1.0);
+		
+		return 0; 
+	
+		
+	}
 }
